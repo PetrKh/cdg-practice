081291ddc0807ca5feea28b56c4ab6a38c7f6ede
PetrKh <56516954+PetrKh@users.noreply.github.com>
2022-11-29 13:15:54 +0300

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -6,3 +6,12 @@ name = gets.to_s
 print "Enter you're age - "
 age = gets.to_i
 print greeting(name,age)
+
+def foobar(ch1,ch2)
+  ch1 == 20? ch2 : ch2 == 20? ch1 : ch1+ch2
+end
+print "Enter number 1 - "
+ch1 = gets.to_i
+print "Enter number 2 - "
+ch2 = gets.to_i
+puts foobar(ch1,ch2)

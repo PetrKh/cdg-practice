3d3319c679fb5c2769fc033a23f968cbd1e49175
PetrKh <56516954+PetrKh@users.noreply.github.com>
2022-11-29 12:17:57 +0300

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,1 +1,8 @@
-puts "hello world"
+def greeting(name, age)
+  age<18? "Привет, #{name}. Тебе меньше 18 лет, но начать учиться програмировать никогда не рано" : "Привет, #{name}. Самое время заняться делом"
+end
+print "Enter you're name and lasname - "
+name = gets.to_s
+print "Enter you're age - "
+age = gets.to_i
+print greeting(name,age)
